418bee862bb6f17fb37ba748c66949be8652d2c6
Karl Ryan <kryan.ie1986@gmail.com>
2023-05-10 16:08:38 +0000

    add basicattack formula

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
+var playerHP = 100;
+var playerMP = 100;
+var dragonHP = 100;
+var vampireHP = 100;
+var demonHP = 100;
+
+
 function replace () {
     document.getElementById('content').innerHTML = "This is the replacement text";
 }
 
 let button = document.getElementById('button');
-button.addEventListener('click', replace);
+button.addEventListener('click', attack);
+
+function attack () {
+    let attackDamage = Math.floor(Math.random() * 10)
+    dragonHP = dragonHP - attackDamage;
+    console.log(dragonHP);
+    alert(dragonHP);
+}
