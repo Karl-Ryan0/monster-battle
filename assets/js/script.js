f9469d983cfa5a85831d0da0fcdbb0c484d2b4ad
Karl Ryan <kryan.ie1986@gmail.com>
2023-05-10 16:21:36 +0000

    ad more complex methods to attack formula

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var playerMP = 100;
 var dragonHP = 100;
 var vampireHP = 100;
 var demonHP = 100;
+var potion = 3;
 
 
 function replace () {
@@ -13,8 +14,15 @@ let button = document.getElementById('button');
 button.addEventListener('click', attack);
 
 function attack () {
-    let attackDamage = Math.floor(Math.random() * 10)
-    dragonHP = dragonHP - attackDamage;
-    console.log(dragonHP);
-    alert(dragonHP);
+    let attackDamage = Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10 );
+    if (attackDamage <= 5) {
+        console.log("you missed!")
+        attackDamage = 0;
+    } else {
+        dragonHP = dragonHP - attackDamage;
+    }
+    console.log(`${attackDamage} damage done, ${dragonHP} HP remains.`);
+    if (dragonHP <= 0 ) {
+        alert("You beat the Dragon. Your last attack did " + attackDamage + " damage")
+    }
 }
