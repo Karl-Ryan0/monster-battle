c5a55b81bffea1efbf26deec485663e0d3f74f23
Karl Ryan <kryan.ie1986@gmail.com>
2023-05-10 17:09:41 +0000

    ad potion button and function

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,9 @@ attackButton.addEventListener('click', attack);
 let magicButton = document.getElementById('magicButton');
 magicButton.addEventListener('click', magicAttack);
 
+let potionButton = document.getElementById('potionButton');
+potionButton.addEventListener('click', potion);
+
 function attack () {
     let attackDamage = Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10 );
     if (attackDamage <= 5) {
@@ -24,11 +27,12 @@ function attack () {
     } else {
         dragonHP = dragonHP - attackDamage;
     }
-    console.log(`${attackDamage} damage done to enemy, ${dragonHP} HP remains.`);
+    console.log(`${attackDamage} Attack damage done to enemy, ${dragonHP} HP remains.`);
     if (dragonHP <= 0 ) {
         alert(`You beat the Dragon. Your last attack did ${attackDamage} damage`)
-    }
+    } else {
     monsterAttack();
+    }
 }
 
 function monsterAttack() {
@@ -46,16 +50,30 @@ function monsterAttack() {
 }
 
 function magicAttack () {
-    let attackDamage = Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10 );
+    let attackDamage = Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10 ) + 5;
     if (attackDamage >= 15) {
         console.log("Critical hit!")
         attackDamage = attackDamage + 20;
+        dragonHP = dragonHP - attackDamage;
     } else {
         dragonHP = dragonHP - attackDamage;
     }
-    console.log(`${attackDamage} damage done to enemy, ${dragonHP} HP remains.`);
+    playerMP = playerMP - 10;
+    console.log(`${attackDamage} Magic damage done to enemy, ${dragonHP} HP remains. You have ${playerMP} MP remaining.`);
     if (dragonHP <= 0 ) {
         alert(`You beat the Dragon. Your last attack did ${attackDamage} damage`)
-    }
+    } else {
     monsterAttack();
+    }
+}
+
+function potion () {
+    if (potion > 0) {
+        console.log(`You regain ${20} points of health`);
+        playerHP = playerHP + 20;
+        potion --;
+        monsterAttack();
+    } else {
+        console.log('You have no potions left!')
+    }
 }
