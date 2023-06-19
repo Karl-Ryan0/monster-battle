aab921283c4fed22cf62fae3d585cec939a93dee
Karl Ryan <kryan.ie1986@gmail.com>
2023-06-19 15:58:46 +0000

    simplify code for the dice roll

--- assets/js/script.js
+++ assets/js/script.js
@@ -177,24 +177,19 @@ function rollTheDiceStats() {
     document.getElementById("potionButton").style.visibility = "visible";
     document.getElementById("player-stats").innerHTML = "";
     document.getElementById("enemy-stats").innerHTML = "";
+    document.getElementById("content").innerHTML = `You rolled a ${dice}! Your stats have increased.`;
     if (dice == 1) {
-        document.getElementById("content").innerHTML = `You rolled a ${dice}! Your stats have increased.<br>
-        <img src="assets/images/one.png" alt="Dice">`;
+        document.getElementById("content").innerHTML += `<img src="assets/images/one.png" alt="Dice">`;
     } else if (dice == 2) {
-        document.getElementById("content").innerHTML = `You rolled a ${dice}! Your stats have increased.<br>
-        <img src="assets/images/two.png" alt="Dice">`;
+        document.getElementById("content").innerHTML += `<img src="assets/images/two.png" alt="Dice">`;
     } else if (dice == 3) {
-        document.getElementById("content").innerHTML = `You rolled a ${dice}! Your stats have increased.<br>
-        <img src="assets/images/three.png" alt="Dice">`;
+        document.getElementById("content").innerHTML += `<img src="assets/images/three.png" alt="Dice">`;
     } else if (dice == 4) {
-        document.getElementById("content").innerHTML = `You rolled a ${dice}! Your stats have increased.<br>
-        <img src="assets/images/four.png" alt="Dice">`;
+        document.getElementById("content").innerHTML += `<img src="assets/images/four.png" alt="Dice">`;
     } else if (dice == 5) {
-        document.getElementById("content").innerHTML = `You rolled a ${dice}! Your stats have increased.<br>
-        <img src="assets/images/five.png" alt="Dice">`;
+        document.getElementById("content").innerHTML += `<img src="assets/images/five.png" alt="Dice">`;
     } else if (dice == 6) {
-        document.getElementById("content").innerHTML = `You rolled a ${dice}! Your stats have increased.<br>
-        <img src="assets/images/six.png" alt="Dice">`;
+        document.getElementById("content").innerHTML += `<img src="assets/images/six.png" alt="Dice">`;
     } else {
         alert(`You rolled a ${dice}, unexpected result.`)
     }
